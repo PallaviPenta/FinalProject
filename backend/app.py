3985e8234111dfd845fba1c93f54fd17f1483177
Pallavi <110376286+PallaviPenta@users.noreply.github.com>
2025-03-07 14:49:52 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,28 +2,27 @@ import sys
 import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from MalwareDetection import predict_malware  # Ensure correct import
+from MalwareDetection import predict_malware  
 
-# Ensure the backend directory is in Python's path
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/predict": {"origins": "https://final-project-ten-dun-95.vercel.app"}})
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         data = request.json  # Get JSON input from frontend
 
-        # Ensure data contains the 'features' key and is a list
+      
         if not isinstance(data, dict) or "features" not in data or not isinstance(data["features"], list):
             return jsonify({"error": "Expected 'features' key with a list of binary values (0 or 1)."}), 400
 
-        # Ensure all feature values are either 0 or 1
+        
         if not all(value in [0, 1] for value in data["features"]):
             return jsonify({"error": "Feature values must be only 0 or 1."}), 400
 
-        # Call the prediction function with valid input
+        
         result = predict_malware(data["features"])
         return jsonify({"prediction": result})
 
